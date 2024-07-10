dcfa183d2499cc6e8e37928adbbb11c681e2615e
achla dhiman <achladhiman3@gmail.com>
2024-07-10 18:48:13 +0530

    utility code added

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import  express  from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+const corsOptions = {
+    origin: process.env.CROSS_ORIGIN,
+    credentials: true, 
+}
+
+app.use(cors({ corsOptions }))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export { app } //similar to export default app
