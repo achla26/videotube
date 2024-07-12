cea23667a886a2b229c206b38aa24241e2465843
achla dhiman <achladhiman3@gmail.com>
2024-07-12 23:58:57 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -18,9 +18,9 @@ export { asyncHandler }
 //     try {
 //         await fn(req, res, next)
 //     } catch (error) {
-//         res.status(error.code || 500).json({
+//         res.status(err.code || 500).json({
 //             success: false,
-//             message: error.message
+//             message: err.message
 //         })
 //     }
 // }
